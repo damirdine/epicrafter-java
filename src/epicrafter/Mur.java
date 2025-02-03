6d8d5ecedb728f6a45f5f732e6a0f205c78048fb
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2025-02-03 11:45:24 +0100

    add final to args

--- src/epicrafter/Mur.java
+++ src/epicrafter/Mur.java
@@ -3,7 +3,7 @@ package epicrafter;
 public class Mur extends Bloc {
     protected boolean porteur;
 
-    public Mur(int l, boolean porteur) {
+    public Mur(final int l,final boolean porteur) {
         super(l);
         this.porteur = porteur;
     }
